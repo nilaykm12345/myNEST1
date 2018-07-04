5a4e0921728e60dcf6aae28f5eaead277fdd38f6
885382 <885382@01HW1091173.IN.TAPUE.COM>
2018-07-05 02:56:41 +0530

    ok1

--- NEST/src/test/java/library/BaseTest.java
+++ NEST/src/test/java/library/BaseTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -16,8 +17,12 @@ public WebDriverWait wait;
 @BeforeMethod(alwaysRun=true)
 public void launchBrowser()
 {
-	System.setProperty(CHROME_KEY,CHROME_PATH);
-	driver =new ChromeDriver();
+	/*System.setProperty(CHROME_KEY,CHROME_PATH);
+	driver =new ChromeDriver();*/
+	
+	System.setProperty(iexplorer_KEY,iexplorer_PATH);
+	driver = new InternetExplorerDriver();
+	
 	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	driver.manage().window().maximize();
 }
--- NEST/src/test/java/library/Constants.java
+++ NEST/src/test/java/library/Constants.java
@@ -5,4 +5,7 @@ public interface Constants
 
 	String CHROME_KEY="webdriver.chrome.driver";
 	String CHROME_PATH="./driver12/chromedriver.exe";
+	
+	String iexplorer_KEY="webdriver.ie.driver";
+	String iexplorer_PATH="./driver12/IEDriverServer.exe";
 }
